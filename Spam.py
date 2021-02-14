70e0d7bb9423f95b3d3f7af62dcb1706e9a50b30
Tony <54213476+Tony-progr@users.noreply.github.com>
2021-02-14 15:01:36 +0200

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,11 +1,12 @@
 import pyautogui, time
 
-time.sleep(10)
+time.sleep(10) #<-- Take some time to prepare:
 
-f = open("Spam.txt", 'r')
-words = f.readline()
-times_spam = f.readline()
+f = open("Spam.txt", 'r') #<-- Open the Spam.txt file:
+words = f.readline() #<-- Get the massage
+times_spam = f.readline() #<-- How many times it will send the message.
 
+#Start sending:
 for x in range(int(times_spam)):
     pyautogui.press('shiftleft')
     pyautogui.press('enter')
